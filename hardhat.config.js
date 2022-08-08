efdee062d16d78487bc66b6c31dcaab6003e65cc
asteve <asteve@localhost.localdomain>
2022-08-08 12:56:42 +0100

    my dao backend code

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan")
+const dotenv = require("dotenv")
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+};
